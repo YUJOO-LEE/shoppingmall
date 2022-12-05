74793944161bec2b6afa92e79732187df376c0ff
YUJOO-LEE <lllllllllee@gmail.com>
2022-12-05 12:39:01 +0900

    fetcher 생성, 데이터 통신 테스트

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,7 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetcher, QueryKeys } from "../../queryClient";
+
 function ProductList() {
+  const { data } = useQuery({
+    queryKey: [QueryKeys.PRODUCTS], 
+    queryFn: ()=> fetcher({
+        method: 'GET',
+        path: '/products'
+      })
+  })
+  console.log(data);
+  
   return (
     <div>ProductList</div>
-  )
+  );
 }
 
 export default ProductList;
--- src/queryClient.ts
+++ src/queryClient.ts
@@ -2,6 +2,10 @@ import {
   QueryClient,
 } from '@tanstack/react-query';
 
+const BASE_URL = 'https://fakestoreapi.com';
+type TypeMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+type TypeBodyOBJ = { [key: string]: any };
+
 // Create a client
 export const getClient = (()=>{
   let client: QueryClient | null = null;
@@ -10,3 +14,35 @@ export const getClient = (()=>{
     return client;
   }
 })();
+
+export const fetcher = async ({
+  method,
+  path,
+  body,
+  params,
+}: {
+  method: TypeMethod;
+  path: string;
+  body?: TypeBodyOBJ;
+  params?: TypeBodyOBJ;
+}) => {
+  try {
+    const url = `${BASE_URL}${path}`;
+    const fetchOptions: RequestInit = {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': BASE_URL
+      }
+    }
+    const res = await fetch(url, fetchOptions);
+    const json = await res.json();
+    return json;
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+export const QueryKeys = {
+  PRODUCTS: 'PRODUCTS',
+}
