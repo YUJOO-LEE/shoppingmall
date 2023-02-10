7189e1b383256c5b928ffbb5916582affeb329e4
YUJOO-LEE <lllllllllee@gmail.com>
2023-02-10 23:22:31 +0900

    서버 cursor 지정해서 페이지네이션 처리

--- server/src/resolvers/product.ts
+++ server/src/resolvers/product.ts
@@ -2,8 +2,9 @@ import { Resolver } from './types';
 
 const productResolver: Resolver = {
   Query: {
-    products: (parent, args, { db }, info) => {
-      return db.products;
+    products: (parent, { cursor = '' }, { db }, info) => {
+      const fromIndex = db.products.findIndex(product => product.id === cursor) + 1;
+      return db.products.slice(fromIndex, fromIndex + 16) || [];
     },
     product: (parent, { id }, { db }, info) => {
       const found = db.products.find(item => item.id === id);
